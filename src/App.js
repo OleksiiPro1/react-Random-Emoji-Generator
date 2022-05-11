e84e230e51d11796c02cbad6040c42af8007d8bf
Alexey Piltenko <ukr.top@ukr.net>
2022-05-11 07:54:34 +0300

    Emoji

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import nodeEmoji from 'node-emoji';
+import { useState } from 'react';
 
-function App() {
+export default function App() {
+  const [emoji, setEmoji] = useState(nodeEmoji.random().emoji);
+  const [emojiName, setEmojiName] = useState('');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Emoji: {emoji}</div>
+      <button onClick={() => setEmoji(nodeEmoji.random().emoji)}>Random</button>
+      <h2>Choose an emoji</h2>
+      <input
+        value={emojiName}
+        onChange={(event) => setEmojiName(event.currentTarget.value)}
+      />
+      <br />
+      {nodeEmoji.hasEmoji(emojiName)
+        ? nodeEmoji.get(emojiName)
+        : 'Emoji not found'}
     </div>
   );
 }
-
-export default App;
+console.log(nodeEmoji.random().emoji);
